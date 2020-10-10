f61823ec1fb6c55b974c68b200078ea757c5dc4c
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-10-09 22:18:28 -0500

    Adding firts routes

--- node-js/node-express-router/index.js
+++ node-js/node-express-router/index.js
@@ -10,6 +10,30 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next ();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the ishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name +
+    ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operationnot supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
