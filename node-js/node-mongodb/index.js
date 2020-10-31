7877066a51cd49a16d7b2a0416e96c24fa1f3cfb
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-10-30 21:16:04 -0500

    Mongodb exercise with promises

--- node-js/node-mongodb/index.js
+++ node-js/node-mongodb/index.js
@@ -4,32 +4,35 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
 
-mongoClient.connect(url, (err, client) => {
-    assert.strictEqual(err, null);
+mongoClient.connect(url).then ((client) => {
+    
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
     
-    dboper.inserDocument(db, {name: "Burger Bon Jovi", description: "Delicious"}, 'dishes', (result) =>{
-
+    dboper.inserDocument(db, {name: "Burger Bon Jovi", description: "Delicious"}, 'dishes')
+        .then((result) => {
         console.log('Insert Document:\n', result.ops);
+        return dboper.findDocument(db, "dishes");
+})
+.then((docs) => {
+    console.log('Found Documents:\n', docs);
 
-        dboper.findDocument(db, 'dishes', (docs) => {
-            console.log('Found Documents:\n', docs);
+    return dboper.updateDocument(db, {name: 'Burger U2'}, {description: 'Cheap'}, 'dishes');
+})
+.then((result) => {
+    console.log("Updated Document:\n", result.result);
 
-            dboper.updateDocument(db, {name: 'Burger U2'}, {description: 'Cheap'}, 'dishes', (result) => {
-                console.log("Updated Document:\n", result.result);
-
-                dboper.findDocument(db, 'dishes', (docs) => {
-                    console.log('Found Documents:\n', docs);
-
-                    db.dropCollection('dishes', (result) => {
-                        console.log('Dropped Collection: ', result);
-
-                        client.close();
-                    });    
-                });
-            });
-        });
-    });
+    return dboper.findDocument(db, 'dishes');
+})
+.then((docs) => {
+    console.log('Found Documents:\n', docs);
+    return  db.dropCollection('dishes');
+})
+.then((result)=> {
+    console.log('Dropped Collection: ', result);
+    return client.close();
+})
+.catch((error) => {
+    console.log(error);
+})
 })
