c7cbd721b8cc0c9f4d8bc3e5444ffea67d5ca2ee
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-11-01 21:38:41 -0500

    Mongoose Part One

---
+++ node-js/node-mongoose/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=> {
+    console.log('We are connected correctly to the server');
+
+    var newDish = Dishes({
+        name: "AeroSmith Burger",
+        description: "Delicious meat with light bread"
+    });
+
+    newDish.save()
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findOne({}).exec();
+    })
+    .then((dishes)=> {
+        console.log(dishes);
+
+        return Dishes.deleteOne({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((error)=> {
+        console.log(error);
+    })
+});
+
+
+
+
