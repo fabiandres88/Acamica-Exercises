ebce5176a84cf6089ef71cfa661034ffef7f812f
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-11-07 20:29:41 -0500

    Finihing mongoose exercise part 2

--- node-js/node-mongoose/index.js
+++ node-js/node-mongoose/index.js
@@ -5,31 +5,41 @@ const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
-connect.then((db)=> {
+connect.then((db) => {
     console.log('We are connected correctly to the server');
 
     Dishes.create({
         name: "AeroSmith Burger",
         description: "Delicious meat with light bread"
     })
-    .then((dish) => {
-        console.log(dish);
+        .then((dish) => {
+            console.log(dish);
 
-        return Dishes.findOne({}).exec();
-    })
-    .then((dishes)=> {
-        console.log(dishes);
+            return Dishes.findByIdAndUpdate(dish._id,{
+                $set: {description:'AeroSmith'}
+            },
+            {new: true}
+            ).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+            dishes.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Carolina Jaimes'
+            });
 
-        return Dishes.deleteOne({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((error)=> {
-        console.log(error);
-    })
+            return dishes.save();
+
+        })
+        .then((dishes)=> {
+            console.log(dishes);          
+            return Dishes.deleteOne({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 });
-
-
-
-
